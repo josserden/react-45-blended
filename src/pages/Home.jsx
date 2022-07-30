05aa60378994c4214041afbb3d34f1f6205039dd
Maxim Stribylo <max057521@gmail.com>
2022-07-30 15:56:02 +0300

    add Countries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { getCountries } from 'service/country-service';
 
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getCountries();
+
+        setCountries(data);
+      } catch (error) {}
+    };
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
