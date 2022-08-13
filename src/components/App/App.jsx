602f3c2a07e8888c318831aea57d821daa5d0b62
DmitriyGovosyants <Demiangvs@gmail.com>
2022-08-13 15:53:45 +0300

    fix: delete comment

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,6 @@
-// import { useLocalStorage } from 'hooks';
-
 import { Container, Header, SearchForm, Section, TodoList } from 'components';
 
 export const App = () => {
-  // const [todos, setTodos] = useLocalStorage('todos', []);
-
-  // const addTodo = text => {
-  // const todo = {
-  //   id: nanoid(),
-  //   text,
-  // };
-
-  //   setTodos([...todos, todo]);
-  // };
-
-  // const handleSubmit = data => {
-  //   addTodo(data);
-  // };
-
-  // const deleteTodo = id => {
-  //   setTodos(todos.filter(todo => todo.id !== id));
-  // };
-
   return (
     <>
       <Header />
